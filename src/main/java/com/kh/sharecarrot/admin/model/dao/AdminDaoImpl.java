3e5007326e5dd7826208bd410fe51bce85094cf9
jinha <jinhak94@naver.com>
2021-05-16 22:36:42 +0900

    admin

---
+++ src/main/java/com/kh/sharecarrot/admin/model/dao/AdminDaoImpl.java
@@ -0,0 +1,13 @@
+package com.kh.sharecarrot.admin.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AdminDaoImpl implements AdminDao {
+
+	@Autowired
+	private SqlSessionTemplate session;
+	
+}
